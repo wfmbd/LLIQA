4120686cacc0238b7b04ce4c0d87860b0a97e13e
xxx <157002958+wfmbd@users.noreply.github.com>
2025-07-22 16:40:40 +0800

    utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,4 @@
-# Copyright (C) <2022> Xingran Liao
-# @ City University of Hong Kong
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this code and
-# associated documentation files (the "code"), to deal in the code without restriction, including without
-# limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the code,
-# and to permit persons to whom the code is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the code.
-
-# THE CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE Xingran Liao BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE code OR THE USE OR OTHER DEALINGS IN THE code.
-
-#================================================
 import numpy as np
 import torch
 from torchvision import transforms
